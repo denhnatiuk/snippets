9781b84a9dd1f3517c37db165a35df79c534e29f
DenysHnatiuk <denys.hnatiuk@gmail.com>
2020-04-24 12:23:26 +0300

    cleanup

--- gists/deferLoadedImg.js
+++
@@ -1,7 +0,0 @@
-function init() {
-var imgDefer = document.getElementsByTagName('img');
-for (var i=0; i<imgDefer.length; i++) {
-if(imgDefer[i].getAttribute('data-src')) {
-imgDefer[i].setAttribute('src',imgDefer[i].getAttribute('data-src'));
-} } }
-window.onload = init;
