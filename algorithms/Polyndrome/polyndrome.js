fb016f19391e150d9488c100a5bb526402d26f26
DenysHnatiuk <denys.hnatiuk@gmail.com>
2020-04-24 13:38:57 +0300

    Add Polyndrome algorithms

---
+++ algorithms/Polyndrome/polyndrome.js
@@ -0,0 +1,67 @@
+'use strict';
+/**
+ *
+ */
+
+function isPolyndrome(word){
+
+  function checkType(word){
+
+  }
+
+    do {
+
+    } while (true);
+
+
+    // strict check
+    if (typeOf word === String || word instanceOf String){
+        if (word.length % 2 === 0) {}
+        let mid = (word.length / 2).toFixed();
+        let rev = word.slice()
+    } else { return false }
+    // type casting to string
+    let wordArr = word.toString().loLowerCase().split('');
+    let right = wordArr.shift();
+
+
+    // str.split('').reverse().join('')
+    //
+    // let reversed = '';
+    // for (const character of str) {
+    //   reversed = character + reversed
+    // }
+    // return reversed
+    //
+    // str.split('').reduce((reversed, character) => character + reversed, '')
+    //
+    // es6
+    // [...str].reverse().join('')
+    //
+    let arr = Array.from(word);
+    for (let i=0; i < arr.length/2; i++) {
+      if (arr[i] === arr.pop(i)){
+
+      }
+    }
+    // do-while loop
+    do{
+
+    }while{
+
+    }
+
+    // Promise
+    //
+    //
+
+    // switch case
+    // let arr = Array.from(word);
+    switch (expression) {
+      case expression:
+
+        break;
+      default:
+
+    }
+}
