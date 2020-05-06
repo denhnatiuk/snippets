7d8a3d91183d33449ee7efd4f40878e3f3d43fb2
DenysHnatiuk <denys.hnatiuk@gmail.com>
2020-05-06 11:51:47 +0300

    Update ShareAPI usage example

--- Patterns/browser/shareAPI.js
+++ Patterns/browser/shareAPI.js
@@ -1,3 +1,7 @@
+/**
+* Source: https://web.dev/web-share/
+*/
+
 if (navigator.share) {
   navigator.share({
     title: 'web.dev',
@@ -6,4 +10,25 @@ if (navigator.share) {
   })
     .then(() => console.log('Successful share'))
     .catch((error) => console.log('Error sharing', error));
+}
+
+//
+let url = document.location.href;
+const canonicalElement = document.querySelector('link[rel=canonical]');
+if (canonicalElement !== null) {
+    url = canonicalElement.href;
+}
+navigator.share({url: url});
+
+// files sharing
+if (navigator.canShare && navigator.canShare({ files: filesArray })) {
+  navigator.share({
+    files: filesArray,
+    title: 'Vacation Pictures',
+    text: 'Photos from September 27 to October 14.',
+  })
+  .then(() => console.log('Share was successful.'))
+  .catch((error) => console.log('Sharing failed', error));
+} else {
+  console.log(`Your system doesn't support sharing files.`);
 }
